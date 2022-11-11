260c3292a6bdeb0640d9114da18db05edfdb475e
Suraj Ghimire <suraj@ghishadow.com>
2022-11-11 06:06:14 +0530

    switch to solid
    Signed-off-by: Suraj Ghimire <suraj@ghishadow.com>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,6 @@
-import {defineConfig} from 'vite'
-import {svelte} from '@sveltejs/vite-plugin-svelte'
+import solid from "solid-start/vite";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [svelte()]
-})
+  plugins: [solid()],
+});
