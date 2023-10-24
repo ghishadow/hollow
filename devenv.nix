d526e5576914c8f00fdd780265a18ccd3464aca0
Suraj Ghimire <suraj@ghishadow.com>
2023-10-24 21:51:00 +0530

    bump packages and add rustup
    Signed-off-by: Suraj Ghimire <suraj@ghishadow.com>

--- devenv.nix
+++ devenv.nix
@@ -2,10 +2,9 @@
 
 {
 
-  packages = with pkgs; [ git ];
+  packages = with pkgs; [ git rustup ];
 
   languages.nix.enable = true;
-  languages.rust.enable = true;
   languages.javascript.enable = true;
   languages.typescript.enable = true;
 
