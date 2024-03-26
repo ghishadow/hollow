fcb444e51288164d8794f35cf7aa1afad507b38c
Suraj Ghimire <suraj@ghishadow.com>
2024-03-26 15:26:43 +0530

    switch to custom nixpkgs
    Signed-off-by: Suraj Ghimire <suraj@ghishadow.com>

--- devenv.nix
+++ devenv.nix
@@ -1,12 +1,14 @@
-{ pkgs, ... }:
-
-{
-
-  packages = with pkgs; [ git rustup llvmPackages_16.libcxxClang nodePackages.npm ];
+{pkgs, ...}: {
+  env = {
+    # https://kobzol.github.io/rust/rustc/2023/10/21/make-rust-compiler-5percent-faster.html
+    MALLOC_CONF = "thp:always,metadata_thp:always";
+  };
+  packages = with pkgs; [git rustup llvmPackages_17.libcxxClang nodePackages.npm];
 
   languages.nix.enable = true;
   languages.javascript.enable = true;
   languages.typescript.enable = true;
 
+  difftastic.enable = true;
 
 }
